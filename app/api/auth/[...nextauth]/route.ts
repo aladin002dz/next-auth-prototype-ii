777ca0d4d432bd8b02ac77ee484f829320a479d0
aladin002dz <mahfoudh.arous@gmail.com>
2025-03-21 13:03:22 +0100

    setting up prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,3 @@
 import { handlers } from "@/auth" // Referring to the auth.ts we just created
+//export const runtime = 'nodejs' // Add this line to fix the Prisma Edge runtime error
 export const { GET, POST } = handlers
